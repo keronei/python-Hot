1ae787164c8693529bbc00171b40adad92402220
keronei <keronei95@gmail.com>
2018-06-25 12:22:24 +0300

    Commented the code for clarity

--- hot.py
+++ hot.py
@@ -1,7 +1,13 @@
+#basic system commands to the program
 import sys
 
+#main function definition 
 def main():
-	print 'Hello there',sys.argv[1]
+	"""output something combined with the input
+	provided by the user
+	"""
+	print 'Hello, You are',sys.argv[1]
 
 if __name__ == '__main__':
+	#call the function
 	main()
